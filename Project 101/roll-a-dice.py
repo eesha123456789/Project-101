666b2c0131bd0d1da876dd7cd8d8029cce2d1772
tejeesha <73623905+tejeesha@users.noreply.github.com>
2022-08-02 20:34:37 -0700

    Add files via upload

---
+++ Project 101/roll-a-dice.py
@@ -0,0 +1,32 @@
+import random
+response="y"
+print("hi")
+while response=="y":
+    no=random.randint(1,6)
+    print("[-----]")
+    if(no==1): 
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+    if(no==2): 
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+    if(no==3): 
+        print("[     ]")
+        print("[0 0 0]")
+        print("[     ]")
+    if(no==4): 
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+    if(no==5): 
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+    if(no==6): 
+        print("[0 0 0]")
+        print("[     ]")
+        print("[0 0 0]")
+    print("[-----]")
+    response=input("Press y to roll agin and n to exit:")
